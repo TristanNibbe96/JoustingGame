e29bf98630e16f1d7a6e7817d56aab823b02a781
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-03-26 18:19:28 -0700

    work done so far

---
+++ Assets/Targetcontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Targetcontroller : MonoBehaviour
+{
+    private Image targetImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void PointerEntered()
+    {
+        targetImage.color = Color.green;
+    }
+
+
+    public void PointerExited()
+    {
+        targetImage.color = Color.red;
+    }
+}
